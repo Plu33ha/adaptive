0ef8041379d887828bf8db63c65ddd11faa182e3
Igor Kot <kigor2388@gmail.com>
2021-12-24 13:30:02 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let burger = document.querySelector("#burger-icon");
+            let header = document.querySelector("#header");
+            burger.onclick = function () {
+                header.classList.toggle("menu-open");
+            };
+$(document).ready(function(){
+	$('.slider').slick({
+		arrows:false,
+		dots:true,
+		adaptiveHeight:true,
+		slidesToShow:1,
+		slidesToScroll:1,
+		speed:1000,
+		easing:'ease',
+		
+	});
+});
